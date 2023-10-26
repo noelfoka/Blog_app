219ff5fc72d60ab09f898746ab85e3373e44d7ce
noelfoka <nomgnenoel@gmail.com>
2023-10-26 17:17:39 +0100

    Added validation for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   attribute :photo, :string
   attribute :posts_counter, :integer, default: 0
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def three_must_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
