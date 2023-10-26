3bae323ff654427110c897ce5d195695dc759835
noelfoka <nomgnenoel@gmail.com>
2023-10-26 03:27:29 +0100

    Create models classes for like and comment

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,4 @@
-class Comment <ApplicationRecord
+class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
-
-  attribute :text, type: Text
 end
