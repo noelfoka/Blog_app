39d37bfcb5e1161281307cd77899fa3345eb64e5
noelfoka <nomgnenoel@gmail.com>
2023-10-26 03:32:47 +0100

    fix the linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class Comment <ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
