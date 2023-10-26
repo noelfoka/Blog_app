f082f099e2bf93e5ff11dc067c434c58b9cd446b
noelfoka <nomgnenoel@gmail.com>
2023-10-26 06:12:18 +0100

    create a method that updates the comments counter for a post.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   attribute :text, :text
+
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
