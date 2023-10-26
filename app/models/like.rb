0f5af3ac21d58b4e22e2d374a6a995512cf3a0dd
noelfoka <nomgnenoel@gmail.com>
2023-10-26 06:14:26 +0100

    create a method that updates the likes counter for a post.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  after_save :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
