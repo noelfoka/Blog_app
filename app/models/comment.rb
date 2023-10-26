e8e759d2d2ac44d27ba914d48600ca7f8216bf92
noelfoka <nomgnenoel@gmail.com>
2023-10-26 06:23:13 +0100

    fix error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   attribute :text, :text
