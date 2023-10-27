e015307f568dc5be079b3283d3fbd2dd4f9794a3
noelfoka <nomgnenoel@gmail.com>
2023-10-27 09:05:41 +0100

    create all the validations and make the test for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
-  has_many :comments
+  has_many :commets
   has_many :likes
 
-  attribute :name, type: String
+  attribute :name, :string
   attribute :bio, :text
+  attribute :post_counter, :integer, default: 0
   attribute :photo, :string
-  attribute :posts_counter, :integer, default: 0
 
-  def three_must_recent_posts
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
