ab90bbe964b34bce38d9b0c3f30cf271d1b2bdd5
noelfoka <nomgnenoel@gmail.com>
2023-10-30 11:21:18 +0100

    create controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      @posts = Post.all
+      @post = Post.find params[:id]
+      @post_id = @posts.index(@post) + 1
+    end
+  end
