4ef703b6b4a17ebfa709ea27706f7f79a500bf85
noelfoka <nomgnenoel@gmail.com>
2023-11-01 14:13:17 +0100

    start the implementation for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
- 
-  resources :users, only: [:index, :show] do
-    resources :posts , only:[:index,:show], controller: 'user_posts'
-  end
+  root to: "users#index"
 
-  resources :posts, only: [:index, :show]
-  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
