647ae61b2d272a49a71e3500d441fd2f422f4f35
noelfoka <nomgnenoel@gmail.com>
2023-11-02 15:44:53 +0100

    Implemented the set_current_user method in the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    private
+
+    def set_current_user
+        user_id = params[:user_id]
+        @current_user = User.find_by_id(user_id)
+    end
+
+    def require_user
+        redirect_to root_path unless @current_user
+    end
+
+    attr_reader :current_user
+
+    helper_method :current_user
 end
