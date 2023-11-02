9807dda46fc42d0fa7a50cf4ae867ace2431964f
noelfoka <nomgnenoel@gmail.com>
2023-11-02 21:51:01 +0100

    finish implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  get 'comments/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
- 
-  root to: "user#index"
-
   resources :user, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]do
+    resources :comments ,only:[:index,:new,:create]
+    resources :likes ,only:[:create]
+    end
   end
+
+  # resources :posts, only: [:show]
+  root 'user#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
