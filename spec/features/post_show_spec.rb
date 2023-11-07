bb7040015f2041eb8f99fa66159c19a463d6109e
noelfoka <nomgnenoel@gmail.com>
2023-11-07 04:21:57 +0100

    update

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -22,36 +22,5 @@ RSpec.feature 'Post Show', type: :feature do
   end
 end
 
-RSpec.feature 'Post Show', type: :feature do
-  let(:user) do
-    User.create(name: 'Noel', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                bio: 'He is a good programmer')
-  end
-  let!(:post) { Post.create(author: user, title: "first post's title", text: 'first text') }
-  let!(:comment1) { Comment.create(author: user, post:, text: 'first comment') }
-  let!(:comment2) { Comment.create(author: user, post:, text: 'second comment') }
-  let!(:like1) { Like.create(user:, post:) }
-
-  before do
-    visit user_post_path(user, post)
-  end
-
-  scenario "see the post's body" do
-    expect(page).to have_content('first text')
-  end
-
-  scenario 'see the comments and likes' do
-    expect(page).to have_content('first comment')
-    expect(page).to have_content('second comment')
-  end
-
-  scenario 'see the username of each commentor' do
-    user2 = User.create(name: 'Alice')
-    Comment.create(author: user2, post:, text: 'third comment')
-
-    visit user_post_path(user, post)
-
-    expect(page).to have_content('Noel')
-    expect(page).to have_content('Alice')
-  end
+# Paste your code here
 end
