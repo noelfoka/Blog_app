c55c6cecc2e31e7add78e874dc709178057bcbc0
noelfoka <nomgnenoel@gmail.com>
2023-11-07 14:24:17 +0100

    fix linter error

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'User Show', type: :feature do
+RSpec.feature 'User Index', type: :feature do
   let(:user) { User.create(name: 'Noel', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'He is a good programmer') }
   let!(:post1) { Post.create(author: user, title: 'Post #1', text: 'first text') }
   let!(:post2) { Post.create(author: user, title: 'Post #2', text: 'second text') }
@@ -29,16 +29,14 @@ RSpec.feature 'User Show', type: :feature do
 
   scenario 'Redirects to the post show page when a post is clicked' do
     visit user_path(user)
-
     click_link post2.title
     expect(current_path).to eq(user_post_path(user_id: post2.author_id, id: post2.id))
   end
 
   scenario 'displays the number of posts the user has written' do
     visit user_path(user)
-    puts "#{user.post_counter}"
-    expect(page).to have_content("#{user.post_counter}")
-  end  
+    expect(page).to have_content(user.post_counter.to_s)
+  end
 
   scenario 'clicking on See All Posts redirects to the user\'s post index page' do
     click_link 'See All Posts'
