8c65c7a7e7217be3b9087e08b3f68beb81483488
noelfoka <nomgnenoel@gmail.com>
2023-11-08 02:57:40 +0100

    install all dependencies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_current_user
-    user_id = params[:user_id]
-    @current_user = User.find_by(id: user_id)
-  end
-
   def require_user
     redirect_to root_path unless @current_user
   end
