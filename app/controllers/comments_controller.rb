8aa65b970e6f943ca6a99060c340cba9b07e7221
noelfoka <nomgnenoel@gmail.com>
2023-11-09 15:57:21 +0100

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,13 +18,6 @@ class CommentsController < ApplicationController
     end
   end
   # destroy the comment
-  def destroy
-    @comment = Comment.find(params[:id])
-    @post = @comment.post
-    @post.decrement!(:comments_counter)
-    @comment.destroy
-    redirect_to user_post_path(author_id: @post.author_id, id: @post.id), notice: 'Comment successfully deleted'
-  end
 
   private
 
