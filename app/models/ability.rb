59ed7d2f7060ee0f62b33c28beb2c56b88dba16a
rbd3 <rabedesanaandry01@gmail.com>
2023-11-09 10:03:47 +0300

    Define abilities for the user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,27 @@
 class Ability
   include CanCan::Ability
 
-  # add your code here please
+  def initialize(user)
+    # Define abilities for the user here. For example:
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :update, Post do |post|
+        post.author == user
+      end
+      can :destroy, Post do |post|
+        post.author == user
+      end
+      can :update, Comment do |comment|
+        comment.user == user
+      end
+      can :destroy, Comment do |comment|
+        comment.user == user
+      end
+      can :create, Post
+      can :create, Comment
+      can :read, :all
+    end
+  end
 end
