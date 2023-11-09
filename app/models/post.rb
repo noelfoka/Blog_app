20bab38289e1e5cb1b126a30b825e562782e5a30
noelfoka <nomgnenoel@gmail.com>
2023-11-09 17:06:02 +0100

    fix the second require change

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,6 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_user_post_counter
-  after_destroy :update_user_post_counter
 
   def update_user_post_counter
     author.update(post_counter: author.posts.count)
